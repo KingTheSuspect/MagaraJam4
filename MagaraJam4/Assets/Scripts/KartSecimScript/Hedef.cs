ed384030d48ef7de40f0cb906b9ea87a29f1fd77
MuKeDa <m.kemaldrk@gmail.com>
2022-02-19 22:19:57 +0300

    upd

---
+++ MagaraJam4/Assets/Scripts/KartSecimScript/Hedef.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hedef : MonoBehaviour
+{
+    public GameObject kart;
+    public GameObject umut;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "amac")
+        {
+            Instantiate(umut, new Vector2(0, 0), new Quaternion(0, 0, 0, 0));
+            Destroy(kart);
+            Destroy(this.gameObject);
+           
+           
+        }
+    }
+}
