fb617e53ea996087427e3cc577d19ea4e06dce42
Furkan Özdemir <58518824+FurkanOzd@users.noreply.github.com>
2022-02-21 00:02:18 +0300

    Add PlayerJump

--- MagaraJam4/Assets/Scripts/Player.cs
+++ MagaraJam4/Assets/Scripts/Player.cs
@@ -16,6 +16,10 @@ public class Player : MonoBehaviour
 
     public static double money = 3.27;
     public float itemGetDistance = 5f;
+    bool onTheGround = false;
+    float yHiz = 0;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float jumpDownSpeed = 15f;
     void Start()
     {
         inventory = transform.GetComponent<Inventory>();
@@ -28,22 +32,35 @@ public class Player : MonoBehaviour
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
-
+        onTheGround = Physics2D.Raycast(transform.position, Vector3.down, 1.5f,LayerMask.GetMask("Ground"));
+        if (Input.GetKeyDown(KeyCode.Space) && onTheGround)
+        {
+            onTheGround = false;
+            yHiz += jumpSpeed;
+        }
+        if (onTheGround)
+            yHiz = 0;
         hAbs = Mathf.Abs(h);
         animator.SetFloat("Speed", hAbs);
         if (h > 0.1f)
         {
             transform.localScale = new Vector2(1, 1);
-            transform.Translate(h * hiz * Time.deltaTime, 0, 0);
+            transform.Translate(h * hiz * Time.deltaTime, yHiz * hiz * Time.deltaTime, 0);
 
         }
         if (h < -0.1f)
         {
             transform.localScale = new Vector2(-1, 1);
-            transform.Translate(h * hiz * Time.deltaTime, 0, 0);
+            transform.Translate(h * hiz * Time.deltaTime, yHiz * hiz*Time.deltaTime, 0);
         }
-        parasayar.text = "$" + money.ToString("0.00"); ;
+        if(h ==0)
+        {
+            transform.Translate(h * hiz * Time.deltaTime, yHiz * hiz * Time.deltaTime, 0);
+        }
+        parasayar.text = "$" + money.ToString("0.00");
 
+        if(!onTheGround)
+            yHiz -= Time.deltaTime* jumpDownSpeed;
     }
 
 
