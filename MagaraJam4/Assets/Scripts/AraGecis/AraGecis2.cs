465b0977f5e55e95491ff49e69c7d6b37a2ada06
KingTheSuspect <demironurcem@hotmail.com>
2022-02-21 16:45:54 +0300

    Yükleme

---
+++ MagaraJam4/Assets/Scripts/AraGecis/AraGecis2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class AraGecis2 : MonoBehaviour
+{
+    public Animator transition;
+    public float sure;
+    public float sure2;
+    public int scene;
+    public TextMeshProUGUI text;
+
+    public void Start()
+    {
+        text.text = "Bir insanin super gucu kendisi olabilir mi?";
+        StartCoroutine(LoadLevel());
+        
+    }
+    IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(sure);
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(sure2);
+        text.text = "Oynadiginiz icin tesekkurler";
+        transition.SetTrigger("Start2");
+        yield return new WaitForSeconds(sure);
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(sure);
+        SceneManager.LoadScene(0);
+    }
+}
--- MagaraJam4/Assets/Scripts/KartOyunu.cs
+++ MagaraJam4/Assets/Scripts/KartOyunu.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KartOyunu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private bool triggered;
+    public int scene;
+    public Animator transition;
+    IEnumerator LoadLevel()
         
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(scene);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        triggered = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        triggered = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        triggered = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && triggered == true)
+        {
+            StartCoroutine(LoadLevel());
+        }
     }
 }
