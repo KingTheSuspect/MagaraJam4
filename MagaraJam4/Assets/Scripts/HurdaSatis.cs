806491dfcb38eb61e467ad8d006f29c4376ca8ee
KingTheSuspect <demironurcem@hotmail.com>
2022-02-21 11:21:57 +0300

    Yükle

--- MagaraJam4/Assets/Scripts/HurdaSatis.cs
+++ MagaraJam4/Assets/Scripts/HurdaSatis.cs
@@ -6,6 +6,8 @@ public class HurdaSatis : MonoBehaviour
 {
     Item item;
     Player player = null;
+    private bool triggered;
+    public GameObject hurda;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +17,37 @@ public class HurdaSatis : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && triggered == true)
+        {
+            Time.timeScale = 0f;
+            hurda.SetActive(true);
+        }
     }
 
     public void Sat()
     {
         if (item.itemType == Item.ItemType.Trash)
         {
-            player.GetInventory().RemoveItem(Item.ItemType.Trash);
+            player.GetInventory().RemoveItem(item);
             player.uiInventory.RefreshItems(player.GetInventory());
         }
+    }
+    public void Ayril()
+    {
+        Time.timeScale = 1f;
+        hurda.SetActive(false);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        triggered = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        triggered = true;
+    }
+    private void OnTriggerExit2D(Collider other)
+    {
+        triggered = false;
     }
 }
