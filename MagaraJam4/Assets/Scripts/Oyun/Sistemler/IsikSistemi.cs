dc94d9e64b3158ce4121bbb44157bb5b0f5ef933
KingTheSuspect <demironurcem@hotmail.com>
2022-02-25 20:22:15 +0300

    Işık Ayarlama

--- MagaraJam4/Assets/Scripts/Oyun/Sistemler/IsikSistemi.cs
+++ MagaraJam4/Assets/Scripts/Oyun/Sistemler/IsikSistemi.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class IsikSistemi : MonoBehaviour
 {
     public GameObject isiklar;
+    public Light2D lights;
 
     private void Update()
     {
@@ -12,9 +14,29 @@ public class IsikSistemi : MonoBehaviour
         {
             Baslat();
         }
-        if (SaatSistemi.saat <= 17)
+        if (SaatSistemi.saat <= 17 && SaatSistemi.saat > 7)
         {
             Kapat();
+        }
+        if (SaatSistemi.saat >= 9 && SaatSistemi.saat < 12)
+        {
+            lights.intensity = 0.8f;
+        }
+        if (SaatSistemi.saat >= 12 && SaatSistemi.saat < 15)
+        {
+            lights.intensity = 1.5f;
+        }
+        if (SaatSistemi.saat >= 15 && SaatSistemi.saat < 18)
+        {
+            lights.intensity = 0.7f;
+        }
+        if (SaatSistemi.saat >= 18 && SaatSistemi.saat < 21)
+        {
+            lights.intensity = 0.5f;
+        }
+        if (SaatSistemi.saat >= 21 && SaatSistemi.saat < 24)
+        {
+            lights.intensity = 0.3f;
         }
     }
 
